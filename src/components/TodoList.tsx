8a37eae70539a21ca3e6cfd63a16bd42f2f3dd12
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-06-27 14:51:19 +0100

    add functionality for deleting a section

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -61,6 +61,17 @@ export default function TodoList({ todos, setTodos }: TodoListProps) {
     }))
   }
 
+  const deleteSection = (section: string) => {
+    setTodos((currentTodos) => {
+      if (!currentTodos) {
+        return null
+      }
+      // eslint-disable-next-line
+      const { [section]: _, ...rest } = currentTodos
+      return rest
+    })
+  }
+
   return (
     <div>
       {Object.entries(todos).map(([section, tasks]) => (
@@ -68,7 +79,7 @@ export default function TodoList({ todos, setTodos }: TodoListProps) {
           key={section}
           className="section mb-6"
         >
-          <div className="flex gap-2">
+          <div className="flex gap-2 mb-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -83,15 +94,20 @@ export default function TodoList({ todos, setTodos }: TodoListProps) {
                 d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776"
               />
             </svg>
-            <h2 className="capitalize text-lg mb-4">{section}</h2>
+            <h2 className="capitalize text-lg flex-1">{section}</h2>
+            <button aria-label="Delete Section" onClick={() => deleteSection(section)} className="hover:text-error mr-1 self-start">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+              </svg>
+            </button>
           </div>
           <ul>
-            {tasks.map((task) => ( <TodoItem  key={task.id} task={task} section={section} onComplete={handleComplete} onEdit={editTodo} onPriorityChange={handlePriorityChange} onTextInputBlur={handleTextInputBlur} onDelete={deleteTodo}/>))}
+            {tasks.map((task) => (<TodoItem key={task.id} task={task} section={section} onComplete={handleComplete} onEdit={editTodo} onPriorityChange={handlePriorityChange} onTextInputBlur={handleTextInputBlur} onDelete={deleteTodo} />))}
           </ul>
           <TodoCreator todos={todos} setTodos={setTodos} sectionName={section} />
         </section>
       ))}
-      <SectionCreator todos={todos} setTodos={setTodos}/>
+      <SectionCreator todos={todos} setTodos={setTodos} />
     </div>
   );
 }
